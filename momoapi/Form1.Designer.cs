16f7a37c32eb9a144404e56512a537f19d3be7eb
tuann300724 <tuann300724@gmail.com>
2024-10-19 21:35:21 +0700

    first commit

---
+++ momoapi/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace momoapi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(88, 56);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(855, 88);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(607, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập Số (các số ngăn cách nhau 1 khoảng trống):";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(88, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 138);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listView1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(88, 392);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(884, 293);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.HotPink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1027, 728);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ momoapi/Form1.cs
@@ -0,0 +1,146 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace momoapi
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.Columns.Add("Số điện thoại", 100);
+            listView1.Columns.Add("Tên", 400);
+            listView1.Columns.Add("Tổng tiền", 200);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string inputPhones = textBox1.Text; // Get all phone numbers from input
+            string[] phoneNumbers = inputPhones.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries); // Split by comma
+
+            foreach (var phoneNumber in phoneNumbers)
+            {
+                string trimmedPhone = phoneNumber.Trim(); // Trim whitespace
+                if (!string.IsNullOrEmpty(trimmedPhone))
+                {
+                    await LoginAndRetrieveData(trimmedPhone);
+                }
+            }
+        }
+
+        private async Task LoginAndRetrieveData(string phoneNumber)
+        {
+            try
+            {
+                // Step 1: Login and get the token
+                string loginUrl = "https://business.momo.vn/api/authentication/login?language=vi";
+                string loginPayload = JsonConvert.SerializeObject(new
+                {
+                    username = phoneNumber, // Use the current phone number
+                    password = "vanhuY90$"  // Default password
+                });
+
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                    HttpContent content = new StringContent(loginPayload, Encoding.UTF8, "application/json");
+                    HttpResponseMessage loginResponse = await client.PostAsync(loginUrl, content);
+
+                    if (loginResponse.IsSuccessStatusCode)
+                    {
+                        // Step 2: Parse the response to get the token
+                        var loginResponseData = await loginResponse.Content.ReadAsStringAsync();
+                        dynamic loginResult = JsonConvert.DeserializeObject(loginResponseData);
+
+                        string token = loginResult?.data.token; // Adjust field name based on actual API response
+
+                        if (!string.IsNullOrEmpty(token))
+                        {
+                            string merchantUrl = "https://business.momo.vn/api/profile/v2/merchants?requestType=LOGIN_MERCHANTS&language=vi";
+                            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+                            HttpResponseMessage merchantResponse = await client.GetAsync(merchantUrl);
+
+                            if (merchantResponse.IsSuccessStatusCode)
+                            {
+                                var merchantData = await merchantResponse.Content.ReadAsStringAsync();
+                                dynamic merchantResult = JsonConvert.DeserializeObject(merchantData);
+
+                                // Extract merchantId from response
+                                string merchantId = merchantResult?.data.merchantResponseList[0].id; // Adjust based on actual response structure
+                                string merchantName = merchantResult?.data.merchantResponseList[0].brandName;
+
+                                if (!string.IsNullOrEmpty(merchantId))
+                                {
+                                    DateTime today = DateTime.Today; // Get today's date
+                                    DateTime startOfMonth = new DateTime(today.Year, today.Month, 1); // First day of the current month
+                                    DateTime endOfMonth = startOfMonth.AddMonths(1).AddDays(-1);
+                                    string fromDate = startOfMonth.ToString("yyyy-MM-dd"); // Beginning of the day
+                                    string toDate = endOfMonth.ToString("yyyy-MM-dd"); // End of the day
+                                 
+                                    // Step 4: Fetch transaction data
+                                   
+
+                                    string transactionsUrl = $"https://business.momo.vn/api/transaction/v2/transactions/statistics?pageSize=10&pageNumber=0&fromDate={fromDate}T00%3A00%3A00.00&toDate={toDate}T23%3A59%3A59.00&status=ALL&merchantId={merchantId}&language=vi";
+
+                                    HttpResponseMessage transactionResponse = await client.GetAsync(transactionsUrl);
+
+                                    if (transactionResponse.IsSuccessStatusCode)
+                                    {
+                                        var transactionData = await transactionResponse.Content.ReadAsStringAsync();
+                                        dynamic transactions = JsonConvert.DeserializeObject(transactionData);
+                                        string total = transactions.data.totalSuccessAmount;
+
+                                        // Add data to the ListView
+                                        ListViewItem item = new ListViewItem(phoneNumber);
+                                        item.SubItems.Add(merchantName);
+                                        item.SubItems.Add(total + "đ");
+                                        listView1.Items.Insert(0, item);
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show($"Failed to retrieve transactions for {phoneNumber}.");
+                                    }
+                                }
+                                else
+                                {
+                                    MessageBox.Show($"Merchant ID not found for {phoneNumber}.");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show($"Failed to retrieve merchant information for {phoneNumber}.");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Login successful but token not found in response for {phoneNumber}.");
+                        }
+                    }
+                    else
+                    {
+                        string loginErrorResponse = await loginResponse.Content.ReadAsStringAsync();
+                        MessageBox.Show($"Login failed for {phoneNumber}. Status code: {loginResponse.StatusCode}, Response: {loginErrorResponse}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error for " + phoneNumber + ": " + ex.Message);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Logic for handling text input (if needed)
+        }
+        
+    }
+}
